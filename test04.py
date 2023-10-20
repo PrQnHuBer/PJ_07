a9c985fd1d7a9592885d85ea91c4fdcdd5faf3a0
PrQnHuBer <mokil05zaza@hotmail.com>
2023-10-20 12:03:57 +0700

    Vision 2

---
+++ test04.py
@@ -0,0 +1,54 @@
+#  List  คือข้อมูลหลายข้อมูล คนละชนิด ซ้ำกันได้
+        #0  1   2  3    4        5  
+List = [10,20,True,10,'SAU',[20,'IOT']]
+        #6  5   4   3   2        1    คือ -
+#การเข้าถึงข้อมูลใน list เข้าถึงแต่ละข้อมูล เข้าถึงทีละหลายข้อมูล เข้าถึงข้อมูลทั้งหมด
+#เข้าถึงแต่ละข้อมูลใน list
+ 
+print(List[4])
+print(List[-2])
+print(List[5])
+print(List[-1])
+print(List[5][1])
+
+#เข้าถึงหลายข้อมูลใน list
+
+print(List[1:4])
+print(List[-5:-2])
+print(List[3:])
+print(List[:3])
+
+#เข้าถึงข้อมูลทั้งหมด for in
+
+for info in List :
+    print(info, end=',')
+
+print(List)
+List[4] = 'Thailand'
+print(List)
+
+#list method
+
+List.append ('wow')
+List.append ([10,20,30])
+print(List)
+
+List.extend(['A','B'])
+print(List)
+
+#เอาออกแค่ตัวแรกที่เจอ
+List.remove(10)
+List.remove('SAU')
+List.remove(['10,20,30'])
+print(List)
+
+List.pop()
+List.pop()
+List.pop()
+print(List)
+
+#List funcion -> len(),min(),max()
+List2 = [10,20,10,30,True]
+print(len(List2))
+print(min(List2))
+print(max(List2))
---
+++ test05.py
@@ -0,0 +1,37 @@
+# Tuple  คือข้อมูลหลายข้อมูล คนละชนิด ซ้ำกันได้ แต่แก้ไม่ได้ เพิ่มไม่ได้ลดไม่ได้
+        #0  1   2  3    4        5  
+Tuple = (10,20,True,10,'SAU',(20,'IOT'))
+        #6  5   4   3   2        1    คือ -
+
+#เข้าถึงข้อมูลเดียว
+
+print(Tuple[4])
+print(Tuple[-2])
+print(Tuple[5])
+print(Tuple[-1])
+print(Tuple[5][1])
+
+#เข้าถึงหลายข้อมูล
+
+print(Tuple[1:4])
+print(Tuple[-5:-2])
+print(Tuple[3:])
+print(Tuple[:3])
+
+#เข้าถึงข้อมูลทั้งหมด
+
+for info in Tuple :
+    print(info, end=',')
+
+print(Tuple)
+
+List =list(Tuple)
+List[4]="IOT"
+Tuple=tuple(List)
+print(Tuple)
+
+#Tuple functions
+
+print(Tuple.count(10))
+print(Tuple.index["SAU"])
+
